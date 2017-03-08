c11916c35c9198c466c885ff50e61bac6bec3169
Taras Dolynyuk <trsdln@gmail.com>
2017-03-08 15:43:03 +0200

    Adjust deps

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-    name: 'wiseguyeh:azure-resource-office-365',
-    summary: "Allows users to authorize themselves against the office 365 rest APIs",
-    version: "1.0.0",
-    git: "https://github.com/djluck/azure-resource-office-365"
+  name: 'jss:azure-resource-office-365',
+  summary: "Allows users to authorize themselves against the office 365 rest APIs",
+  version: "1.0.1",
+  git: "https://github.com/JSSolutions/azure-resource-office-365"
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.1');
-  api.use(['wiseguyeh:azure-active-directory@1.0.0', 'underscore'], ['server']);
+  api.use(['jss:azure-active-directory@1.0.1', 'underscore'], ['server']);
   api.addFiles('office365Resource.js', 'server');
 });
